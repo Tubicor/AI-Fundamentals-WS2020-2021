735b571a4c560a7481ad3eaa9e3f4e2e972a912e
Tubicor <72609080+Tubicor@users.noreply.github.com>
2020-11-30 18:44:46 +0100

    Improved broadcasting and matrix multiplication, furthermore applied changes recommended by Mister Luczak

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,6 +1,5 @@
 from abc import ABC,abstractmethod
 import numpy as np
-from Task2 import PlotModes
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
@@ -41,12 +40,36 @@ class Gui():
         rm1 = np.ones(r1.shape)*-1
         # horizontal stack vectors to create x1,x2 input for the model
         grid = np.hstack((r1,r2))
-        yhat = np.sum(network.compute(grid),axis=1)/2
+        ###print("compute:",network.compute(grid))
+        yhat = np.sum(network.compute(grid),axis=1)
+        ###yhat = np.array([ (1-i[0] +i[1])/2 for i in network.compute(grid)])
+        ###print("yhat",yhat)
+        #hat = network.compute(grid)
         # reshape the predictions back into a grid
         zz = yhat.reshape(xx.shape)
         # plot the grid of x, y and z values as a surface
-        self.fig.add_subplot(111).contourf(xx, yy, zz)
+        self.fig.add_subplot(111).contourf(xx, yy, zz,2)
         self.canvas.draw()     
+
+class PlotModes:    
+    def __init__(self,numSamples,numModes,fig,variance):
+        self.totalSamples = numSamples*numModes
+        self.samples = np.empty((1,3))#[x,y]
+        meansX = np.random.uniform(0,0.7,numModes)
+        meansY = np.random.uniform(0,0.7,numModes)
+        variances = np.random.uniform(0,variance,numModes)
+        clasDev = np.random.choice(2,numModes)
+        for i in range(0,numModes):
+            #print(i)
+            samplesX = np.random.normal(meansX[i],np.sqrt(variances[i]),numSamples)
+            samplesY = np.random.normal(meansY[i],np.sqrt(variances[i]),numSamples)
+            for j in range(0,numSamples):
+                color = 'b'
+                if(i%2 == 1):
+                    color = 'r'
+                fig.add_subplot(111).plot(samplesX[j],samplesY[j],color,marker='x')
+                self.samples = np.append(self.samples,np.asarray([[i%2,samplesX[j],samplesY[j]]]),axis=0)
+            self.samples = self.samples[1:self.totalSamples+1]#delete first element  
 
 class Layer(ABC):
     def __inti__(self):
@@ -63,7 +86,7 @@ class Layer(ABC):
 
 class Linear(Layer):    
     def __init__(self,numNeurons,numInput):
-        self.w = np.random.rand(numNeurons,numInput+1)#weights for each neuron for each input + theta for each neuron
+        self.w = np.random.rand(numInput+1,numNeurons)#weights for each neuron for each input + theta for each neuron
         self.grad = []
         self.x = []
         self.numNeurons = numNeurons
@@ -72,28 +95,21 @@ class Linear(Layer):
 
     def forward(self,x):
         #store x and add -1 for theta
-        self.x = np.c_[x,np.ones((len(x)))*-1]        
-        #multiply each weight with the corresponding weight for eacht sample
-        return np.asarray([[singleInput@singleWeight for singleWeight in self.w] for singleInput in self.x])
+        self.x = x      
+        #multiply each weight with the corresponding weight for each sample
+        return self._augment(x) @ self.w
         
     def backward(self,grad):
         #store grad
         self.grad = grad 
-        #multiply grad for each sample of batchsize and connection with corresponding weight
-        prod = np.asarray([[grad[i,j]*self.w[j] for j in range(0,len(self.w))] for i in range(0,len(grad))])
-        #sum all mistakes and remove the missfitting for theta
-        return np.sum(prod,axis=1)[:,0:-1]
+        #remove the theta values for next layer
+        return (grad @ self.w.T)[:,:-1]
     
     def adjust(self,eta):
-        update = []
-        #for each sample of the batch
-        for i in range(0,len(self.x)):
-            #multiply grad with every input for sample i
-            update += [np.array([eta*self.grad[i]*xi for xi in self.x[i]])]
-        #sum all updates up and transpone from shape(weight,neuron) to (neuron,weight)
-        update = np.sum(np.asarray(update),axis=0).transpose()
-        #add the update to the weights of the layer
-        self.w = np.add(self.w,update)
+        self.w -= eta * self._augment(self.x).T @self.grad
+    
+    def _augment(self,arr):
+        return np.c_[arr,np.ones((arr.shape[0]))*-1]    
 
 class Activation(Layer):
     def __init__(self,numNeurons,func,dfunc):
@@ -146,14 +162,18 @@ class Network():
             randIndex = np.random.randint(0,data.totalSamples);
             randSamples = data.samples[randIndex:randIndex+batchsize]
             lastElementOfBatchNum = randIndex+batchsize-data.totalSamples;
+            input = randSamples[:,1:]
+            target = randSamples[:,:1]
             #if the random index makes the batch overstand the Array of the samples add the rest of the beginning of the Array
             if lastElementOfBatchNum > 0:
                 randSamples = np.append(randSamples,data.samples[0:lastElementOfBatchNum],axis=0)
 
             #let the network predict
-            predictions = self.compute(randSamples[:,1:]);
+            predictions = self.compute(input);
             #calculate the error
-            grad = 2*(randSamples[:,:1]-predictions)
+            grad = 2*(predictions-target)
+            #print the Loss
+            print("Loss: ",np.sum((predictions-target)**2))
 
             #backprobagate of the layers
             for layer in self.layers[::-1]:
@@ -201,15 +221,14 @@ if __name__ == "__main__":
     #width of the hidden layer
     width = 16
     #instanciate the Network
-    network = Network(width,Functions[0],Functions[1])
+    network = Network(width,Functions[2],Functions[3])
     #instanciate the random data
-    data = PlotModes(200,5,gui.fig,0.0005)
+    data = PlotModes(100,5,gui.fig,0.0005)
     #shuffel the random data samples
     np.random.shuffle(data.samples)
-    
+
     #train network for 500 iterations with 32 batchsize
     network.train(data.samples,500,32)
-    #print(network.compute(data.samples[:,1:]))
     
     gui.plot_decision_boundary(data.samples,network)
     gui.loop()
