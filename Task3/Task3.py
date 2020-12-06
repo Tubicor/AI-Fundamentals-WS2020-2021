4dcdd918f3e30087dbe71f1863d306ea0e0aeaa8
Jonas Werner <72609080+Tubicor@users.noreply.github.com>
2020-12-06 19:00:30 +0100

    Update Task3.py

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 
 class Gui():
     def __init__(self,FunctionNames,Functions):
+        #Unset
+        self.network = None
+        self.data = None
         #GUI Layout
         self.root = tk.Tk()
         self.plotFrame =tk.Frame(self.root)
@@ -17,17 +20,44 @@ class Gui():
         self.ax1 = self.fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.fig,self.plotFrame)
         self.canvas.get_tk_widget().grid()
-        #GUI dropdownMenu
-        self.currentFunc = tk.StringVar(self.settingFrame)
-        self.funcDropMenu=tk.OptionMenu(self.settingFrame,self.currentFunc,*FunctionNames)
-        self.currentFunc.set(FunctionNames[0])
-        self.funcDropMenu.grid(column=0,row=0)
+        #GUI Number of neurons in Hidden Layer
+        
+        self.batchSize = tk.Entry(self.settingFrame,text="Batchsize")
+        self.batchSize.insert(0,"32")
+        self.batchSize.grid(column=0,row=0)
+        lbatch = tk.Label(self.settingFrame,text="Batchsize                    ")
+        lbatch.grid(column=1,row=0)
+        self.iterations = tk.Entry(self.settingFrame,text="Iterations of Training")
+        self.iterations.insert(0,"5000")
+        self.iterations.grid(column=0,row=1)        
+        lbatch = tk.Label(self.settingFrame,text="Iterations of Training   ")
+        lbatch.grid(column=1,row=1)
+        self.numNeurons = tk.Entry(self.settingFrame,text="")
+        self.numNeurons.insert(0,"16")
+        self.numNeurons.grid(column=0,row=2)
+        lbatch = tk.Label(self.settingFrame,text="Neurons in hidden Layer")
+        lbatch.grid(column=1,row=2)
+        ##GUI dropdownMenu
+        #self.currentFunc = tk.StringVar(self.settingFrame)
+        #self.funcDropMenu=tk.OptionMenu(self.settingFrame,self.currentFunc,*FunctionNames)
+        #self.currentFunc.set(FunctionNames[0])
+        #self.funcDropMenu.grid(column=0,row=0)
+        #GUI Buttons
+        self.buttonApply = tk.Button(self.settingFrame,text="apply & create new network",command= lambda: self.network.changeHiddenLayer(int(self.numNeurons.get()),Functions[2],Functions[3]))
+        self.buttonApply.grid(column=0)
+        self.buttonTrain = tk.Button(self.settingFrame,text="train",command= lambda:self.network.train(self.data.samples,int(self.iterations.get()),int(self.batchSize.get())))
+        self.buttonTrain.grid(column=0)
+        self.buttonEvaluate = tk.Button(self.settingFrame,text="evaluate",command= lambda:self.plot_decision_boundary(self.data.samples,self.network))
+        self.buttonEvaluate.grid(column=0)
+    
     def loop(self):
         self.root.mainloop()
+    def newNetwork(self,width):
+        self.network = Network(width,Functions[2],Functions[3])
     def plot_decision_boundary(self, x, model, steps=1000, cmap='Paired'):
         # define bounds of the domain
-        min1, max1 = x[:, 1].min(), x[:, 1].max()+0.03
-        min2, max2 = x[:, 2].min(), x[:, 2].max()+0.03
+        min1, max1 = x[:, 2].min(), x[:, 2].max()+0.03
+        min2, max2 = x[:, 3].min(), x[:, 3].max()+0.03
         # define the x and y scale
         x1grid = np.arange(min1, max1, 0.01)
         x2grid = np.arange(min2, max2, 0.01)
@@ -41,20 +71,18 @@ class Gui():
         # horizontal stack vectors to create x1,x2 input for the model
         grid = np.hstack((r1,r2))
         ###print("compute:",network.compute(grid))
-        yhat = np.sum(network.compute(grid),axis=1)
-        ###yhat = np.array([ (1-i[0] +i[1])/2 for i in network.compute(grid)])
-        ###print("yhat",yhat)
+        yhat = np.array([ 1-i[0] +i[1] for i in network.compute(grid)])
         #hat = network.compute(grid)
         # reshape the predictions back into a grid
         zz = yhat.reshape(xx.shape)
         # plot the grid of x, y and z values as a surface
         self.fig.add_subplot(111).contourf(xx, yy, zz,2)
-        self.canvas.draw()     
+        self.canvas.draw() 
 
 class PlotModes:    
     def __init__(self,numSamples,numModes,fig,variance):
         self.totalSamples = numSamples*numModes
-        self.samples = np.empty((1,3))#[x,y]
+        self.samples = np.empty((1,4))#[x,y]
         meansX = np.random.uniform(0,0.7,numModes)
         meansY = np.random.uniform(0,0.7,numModes)
         variances = np.random.uniform(0,variance,numModes)
@@ -68,8 +96,8 @@ class PlotModes:
                 if(i%2 == 1):
                     color = 'r'
                 fig.add_subplot(111).plot(samplesX[j],samplesY[j],color,marker='x')
-                self.samples = np.append(self.samples,np.asarray([[i%2,samplesX[j],samplesY[j]]]),axis=0)
-            self.samples = self.samples[1:self.totalSamples+1]#delete first element  
+                self.samples = np.append(self.samples,np.asarray([[i%2,(i+1)%2,samplesX[j],samplesY[j]]]),axis=0)
+        self.samples = self.samples[1:self.totalSamples+1]#delete first element  
 
 class Layer(ABC):
     def __inti__(self):
@@ -106,6 +134,7 @@ class Linear(Layer):
         return (grad @ self.w.T)[:,:-1]
     
     def adjust(self,eta):
+        #print(eta * self._augment(self.x).T @self.grad)
         self.w -= eta * self._augment(self.x).T @self.grad
     
     def _augment(self,arr):
@@ -133,6 +162,21 @@ class Activation(Layer):
 class Network():
     def __init__(self,width,func,dfunc):
         self.rows = 2
+        self.width = width
+        self.layers =[]
+        #first layer 2 Neurons and each 2 inputs
+        self.layers += [Linear(2,2)]
+        self.layers += [Activation(2,func,dfunc)]
+        #secound layer var Neurons and each 2 inputs
+        self.layers += [Linear(width,2)]
+        self.layers += [Activation(width,func,dfunc)]
+        #last layer 2 Neurons and each var inputs
+        self.layers += [Linear(2,width)]
+        self.layers += [Activation(2,func,dfunc)]
+
+    def changeHiddenLayer(self,width,func,dfunc):
+        self.rows = 2        
+        self.width = width
         self.layers =[]
         #first layer 2 Neurons and each 2 inputs
         self.layers += [Linear(2,2)]
@@ -152,26 +196,29 @@ class Network():
 
     def train(self,samples,iterations,batchsize):
         lastElementOfBatchNum = 0
-        etaStart = 0.1
+        etaStart = 5/self.width
+        index = 0
         #for iterations
         for i in range(0,iterations):
             #variable Learningrate
             eta = etaStart-(i/iterations)*etaStart
-            
             #Gets random Batch from samples
-            randIndex = np.random.randint(0,data.totalSamples);
-            randSamples = data.samples[randIndex:randIndex+batchsize]
-            lastElementOfBatchNum = randIndex+batchsize-data.totalSamples;
-            input = randSamples[:,1:]
-            target = randSamples[:,:1]
+            batchSample = samples[index:index+batchsize]
             #if the random index makes the batch overstand the Array of the samples add the rest of the beginning of the Array
-            if lastElementOfBatchNum > 0:
-                randSamples = np.append(randSamples,data.samples[0:lastElementOfBatchNum],axis=0)
+            if index >= len(samples):
+                index = index - len(samples)
+                batchSample = np.append(batchSample,samples[0:index],axis=0)
 
+            input = batchSample[:,2:]
+
+            target = batchSample[:,:2]
+            #print("target",target)
             #let the network predict
-            predictions = self.compute(input);
+            predictions = self.compute(input)
+            #print("prediction",predictions)
             #calculate the error
             grad = 2*(predictions-target)
+            #print("grad",grad)
             #print the Loss
             print("Loss: ",np.sum((predictions-target)**2))
 
@@ -180,7 +227,8 @@ class Network():
                 grad = layer.backward(grad)
             #adjust the weights of the layer
             for layer in self.layers:
-                layer.adjust(eta)      
+                layer.adjust(eta)
+            index +=batchsize
    
 if __name__ == "__main__":
     FunctionNames=[
@@ -217,19 +265,16 @@ if __name__ == "__main__":
     
     #instanciate the Gui
     gui = Gui(FunctionNames,Functions)
-
     #width of the hidden layer
-    width = 16
+    width = 32
     #instanciate the Network
     network = Network(width,Functions[2],Functions[3])
+    gui.network = network
     #instanciate the random data
-    data = PlotModes(100,5,gui.fig,0.0005)
+    data = PlotModes(100,10,gui.fig,0.0005)
+    gui.data = data
     #shuffel the random data samples
     np.random.shuffle(data.samples)
 
-    #train network for 500 iterations with 32 batchsize
-    network.train(data.samples,500,32)
-    
-    gui.plot_decision_boundary(data.samples,network)
     gui.loop()
-    
+   
